b56c3a6ca83c8e61a856fb828bc4edecd006766f
Harpreet Kaur Arora <harpreet_k_arora@yahoo.com>
2018-11-09 14:20:29 -0500

    Assignment  - skeleton method for length chcker

---
+++ WordLengthChecker/src/main/java/com/checker/LongestWordFinder.java
@@ -0,0 +1,46 @@
+package com.checker;
+
+import org.apache.log4j.Logger;
+
+import com.model.Word;
+
+/**
+ * <h1>LongestWordFinder</h1> LongestWordFinder class provides a utility method
+ * to find the longest word and its length for a given sentence.
+ * <p>
+ *
+ * @author Harpreet Kaur
+ * @version 1.0
+ * @since 2018-11-09
+ */
+public class LongestWordFinder {
+
+	// Logger for the class
+	static Logger logger = Logger.getLogger(LongestWordFinder.class);
+
+	/**
+	 * Main is to bootstrap the application.
+	 * 
+	 * @param This method takes in the input sentence as an argument and prints the
+	 *             longest word and its length
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	/**
+	 * This method is used to find the longest word and its length for a given input
+	 * sentence.
+	 * 
+	 * @param inputSentence This is input sentence to be parsed by this method
+	 * @return Word This returns the longest word in the input sentence
+	 */
+
+	public static Word lengthChecker(String inputSentence) {
+		return null;
+	
+	}
+
+}
